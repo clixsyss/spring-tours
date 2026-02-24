90a003b234dce5f61563913470b8818244cd4a25
Hady Korshem <itshadyy@gmail.com>
2026-02-25 00:46:26 +0200

    Add react-icons dependency, enhance App.css with new styles for destination items, and update Home.js to display a list of top destinations with improved layout and buttons.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,5 @@
+import { FaClock } from "react-icons/fa";
+
 function Home() {
     return (
         <div className="home">
@@ -8,8 +10,41 @@ function Home() {
 
             <div className="gap"></div>
 
-            <div>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
+            <div className="content-container">
+                <div className="destinations-container">
+                    <h1>Top Destinations</h1>
+                    <div className="destinations-list">
+                        <div className="destination-item">
+                            <div className="destination-item-content">
+                                <h4>Nile Meets the Mediterranean Sea</h4>
+                                <div className="destination-item-description">
+                                    <p> <span><FaClock size={16} /></span> 3 Days 2 Nights</p>
+                                    <button className="btn">Explore More</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="destination-item">
+                            <div className="destination-item-content">
+                                <h4>4-night Cruise Luxor - Aswan</h4>
+                                <div className="destination-item-description">
+                                    <p> <span><FaClock size={16} /></span> 5 Days 4 Nights</p>
+                                    <button className="btn">Explore More</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="destination-item">
+                            <div className="destination-item-content">
+                                <h4>A little Bit Off The Beaten Track</h4>
+                                <div className="destination-item-description">
+                                    <p> <span><FaClock size={16} /></span> 10 Days 9 Nights</p>
+                                    <button className="btn">Explore More</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="right-side-button"> <button className="btn">Explore All Destinations</button> </div>
+                </div>
             </div>
         </div>
     );
