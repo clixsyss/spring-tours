48e6ae7e0e20b357339d01a7522dcc714d724ef3
Hady Korshem <itshadyy@gmail.com>
2026-02-24 21:51:22 +0200

    Update index.html meta tags for SEO, enhance App.css with new styles, and modify Home.js to include a hero section with updated content.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,16 @@
 function Home() {
     return (
         <div className="home">
-            <h1>Home</h1>
+            <div className="hero">
+                <h1>Experience Makes The Difference</h1>
+                <h2>- Since 1975 -</h2>
+            </div>
+
+            <div className="gap"></div>
+
+            <div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
+            </div>
         </div>
     );
 }
